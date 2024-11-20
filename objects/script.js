95bfd816ec41aa455272110fe2db4cc596de9868
Suresh <shansuresh25@gmail.com>
2024-11-20 20:32:36 +0530

    added simple calculator program in script.js

--- objects/script.js
+++ objects/script.js
@@ -1,1 +1,20 @@
 console.log('objects in js')
+
+// calculator program using object - 20/11/2024
+
+calculator = {
+    a,
+    b,
+    read(){
+        this.a = prompt('Enter a number ', '');
+        this.b = prompt('Enter another number ', '');
+    },
+
+    sum(){
+        return this.a + this.b;
+    },
+
+    mul(){
+        return this.a * this.b;
+    }
+}
